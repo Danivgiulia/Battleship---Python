093a7ef5a448543b50e63e68947c2d16212b1ac4
Danivgiulia <danielavgiulianelli@gmail.com>
2025-04-16 15:08:50 +0100

    defined print_board so that it gives me 8x8 board

--- battleship.py
+++ battleship.py
@@ -6,8 +6,14 @@ GUESS_BOARD = [[' '] * 8 for x in range{8}]
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, }
 
 
-def board():
-    pass
+def print_board(board):
+    print (' A B C D E F G H')
+    print ('________________')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+     
 
 def create_ships():
     pass
@@ -17,4 +23,8 @@ def get_ship_location():
 
 def count_hit_ships():
     pass
+
+create_ships()
+turns = 10 
+#  While turns > 0
 

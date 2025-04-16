8904c2850a013ce9ff70192c175219a2e759f00d
Danivgiulia <danielavgiulianelli@gmail.com>
2025-04-16 15:32:30 +0100

    ran program, and fixed syntax error

--- battleship.py
+++ battleship.py
@@ -1,11 +1,17 @@
+# LEGEND
 
+# 'X' is for placing ship and hit battleship]
+# ' ' is for available space
+# '-' is for missed shot
 
-HIDDEN_BOARD = [[' '] * 8 for x in range{8}]
-GUESS_BOARD = [[' '] * 8 for x in range{8}]
+from random import randint
+
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, }
 
-
+# function for printing board
 def print_board(board):
     print (' A B C D E F G H')
     print ('________________')
@@ -14,17 +20,39 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
      
+# function for creating the ships randomly
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0,7), randint(0,7)
+        board[ship_row][ship_column] = 'X'
 
-def create_ships():
-    pass
-
+# function for getting the ship location 
+# bug for if no input then crash
 def get_ship_location():
-    pass
+    row = input('Please enter a ship row 1-8')
+    while row not in '12345678':
+        print('Please enter a valid row')
+        row = input('Please enter a ship row 1-8')
+    column = input('Please enter a ship column A-H').upper()
+    while column not in 'ABCDEFGH':
+        print('Please enter a valid column')
+        column = input('Please enter a ship column A-H').upper()
+    return int(row) -1, letters_to_numbers[column]
 
-def count_hit_ships():
-    pass
 
-create_ships()
+def count_hit_ships(board):
+    count = 0 
+    for row in board:
+        for column in row:
+            if column == 'X':
+                count += 1
+    return count 
+
+create_ships(HIDDEN_BOARD)
 turns = 10 
+print_board(HIDDEN_BOARD)
+print_board(GUESS_BOARD)
 #  While turns > 0
 
